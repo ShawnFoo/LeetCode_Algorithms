2919c6a087a9ebb36e4ff7cf375218ce9d2eebcf
ShawnFoo <fu4904@gmail.com>
2018-03-05 22:44:09 +0800

    Reverse Integer

---
+++ Swift/Reverse_Integer.playground/Contents.swift
@@ -0,0 +1,24 @@
+/*
+Reverse Integer
+
+Tag: Math
+
+https://leetcode.com/problems/reverse-integer/description/
+*/
+
+func reverse(_ x: Int) -> Int {
+	if (x < 0) {
+		return -reverse(-x);
+	}
+	var num = x
+	var reversed = 0
+	while num > 0 {
+		reversed *= 10
+		if reversed > Int32.max {
+			return 0
+		}
+		reversed += num % 10
+		num /= 10
+	}
+	return reversed
+}

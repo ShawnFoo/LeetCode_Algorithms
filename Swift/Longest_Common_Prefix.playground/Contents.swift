51c55b27545a2b8656a38cb7dc9aed6197e1243f
ShawnFoo <fu4904@gmail.com>
2018-03-12 22:32:34 +0800

    Longest Common Prefix 1. Added solution 2. Fixed indent width

--- Swift/Longest_Common_Prefix.playground/Contents.swift
+++ Swift/Longest_Common_Prefix.playground/Contents.swift
@@ -6,45 +6,57 @@ Tag: String
 https://leetcode.com/problems/longest-common-prefix/description/
 */
 
-/*
-思路: 暴力法.
+/**
+思路: 暴力法一
 
-因为是求所有字符串的公共前缀, 所以当存在所有字符串的公共前缀时, 任意两个字符串的公共前缀 一定包含 所有字符的公共前缀.
+因为是求所有字符串的公共前缀, 所以当存在所有字符串的公共前缀时, 任意两个字符串的公共前缀 一定包含 全部字符的公共前缀.
 
 所以 我们可以先求得数组前两个元素的公共前缀, 然后用它去与剩余元素求公共前缀即可.
 */
 func longestCommonPrefix(_ strs: [String]) -> String {
-	if strs.count == 0 {
-		return ""
-	}
-	else if strs.count == 1 {
-		return strs[0]
-	}
-	var commons: [Character] = []
-	let s0 = Array(strs[0])
-	let s1 = Array(strs[1])
-	var i = 0
-	while i < min(s0.count, s1.count) { // 找出头两个字符串的公共前缀
-		if s0[i] != s1[i] {
-			break
-		}
-		commons.append(s1[i])
-		i += 1
-	}
-	// 用前边求得公共前缀 与 剩余字符串匹配, 去除非公共字符, 剩下的即是公共前缀
-	for j in 2..<strs.count {
-		var k = 0
-		let s = Array(strs[j])
-		while k < min(s.count, commons.count) {
-			if s[k] != commons[k] {
-				break
-			}
-			k += 1
-		}
-		commons = Array(commons[0..<k])
-		if commons.count == 0 {
-			break
-		}
-	}
-	return String(commons)
+    if strs.count == 0 {
+        return ""
+    }
+    var commons: [Character] = Array(strs[0])
+    for i in 1..<strs.count {
+        let s = Array(strs[i])
+        var j = 0
+        while j < min(s.count, commons.count) {
+            if s[k] != commons[k] {
+                break
+            }
+            k += 1
+        }
+        commons = Array(commons[0..<j])
+        if commons.count == 0 {
+            break
+        }
+    }
+    return String(commons)
+}
+
+/**
+思路: 暴力法二.
+
+按位垂直遍历每个字符. 直至同位置某个字符串上的字符不匹配则停止
+
+0 1 2
+a b c
+a b d
+a c d
+*/
+func longestCommonPrefix2(_ strs: [String]) -> String {
+    if strs.count == 0 {
+        return ""
+    }
+    var commons: [Character] = Array(strs.first!)
+    for i in 0..<commons.count {
+        for j in 1..<strs.count {
+            let s = Array(strs[j])
+            if i >= s.count || s[i] != commons[i] {
+                return String(Array(commons[0..<i]))
+            }
+        }
+    }
+    return String(commons)
 }

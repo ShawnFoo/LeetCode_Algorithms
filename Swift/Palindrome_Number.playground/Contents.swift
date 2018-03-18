611b7c33aa15536dbe349884596e49a1b8751d0c
ShawnFoo <fu4904@gmail.com>
2018-03-18 22:45:05 +0800

    Palindrome Number

--- Swift/Palindrome_Number.playground/Contents.swift
+++ Swift/Palindrome_Number.playground/Contents.swift
@@ -14,27 +14,27 @@ https://leetcode.com/problems/palindrome-number/description/
 偶数位的回文数 "123321" 前后部分 "123" == "123"(321的反转数)
 */
 func isPalindrome(_ x: Int) -> Bool {
-	if x < 0 { // 负数一定不是回文数
-		return false
-	}
-	var xx = x
-	var digits: Int = 0 // 位数
-	while xx > 0 {
-		digits += 1
-		xx /= 10
-	}
-	xx = x
-	var num = 0
-	// 知道位数后, 直接中间截断, 后半段进行反转
-	for _ in 0..<digits/2 {
-		num *= 10
-		num += xx % 10
-		xx /= 10
-	}
-	if digits % 2 != 0 { // 奇数位 去除中间数字
-		xx /= 10
-	}
-	// 前半部分数值 若等于 后半部分的反转数数值, 那就是回文数
-	return num == xx
+    if x < 0 { // 负数一定不是回文数
+        return false
+    }
+    var xx = x
+    var digits: Int = 0 // 位数
+    while xx > 0 {
+        digits += 1
+        xx /= 10
+    }
+    xx = x
+    var num = 0
+    // 知道位数后, 直接中间截断, 后半段进行反转
+    for _ in 0..<digits/2 {
+        num *= 10
+        num += xx % 10
+        xx /= 10
+    }
+    if digits % 2 != 0 { // 奇数位 去除中间数字
+        xx /= 10
+    }
+    // 前半部分数值 若等于 后半部分的反转数数值, 那就是回文数
+    return num == xx
 }
 

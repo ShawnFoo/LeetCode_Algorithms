8f7d55e0bdae0928ac1a834d3a0c9764e4a95678
ShawnFoo <fu4904@gmail.com>
2018-03-18 22:39:54 +0800

    Two Sum

--- Swift/Two_Sum.playground/Contents.swift
+++ Swift/Two_Sum.playground/Contents.swift
@@ -7,13 +7,13 @@ https://leetcode.com/problems/two-sum/description/
 */
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-	var numIndexDic: [Int: Int] = [:]
-	for i in 0..<nums.count {
-		let diff = target - nums[i]
-		if numIndexDic.keys.contains(diff) {
-			return [numIndexDic[diff]!, i]
-		}
-		numIndexDic[nums[i]] = i
-	}
-	return []
+    var numIndexDic: [Int: Int] = [:]
+    for i in 0..<nums.count {
+        let diff = target - nums[i]
+        if numIndexDic.keys.contains(diff) {
+            return [numIndexDic[diff]!, i]
+        }
+        numIndexDic[nums[i]] = i
+    }
+    return []
 }

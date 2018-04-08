ab4dcf93172f1579880ab7adc04ea748db8704c5
ShawnFoo <fu4904@gmail.com>
2018-04-08 22:32:36 +0800

    3Sum

--- Swift/3Sum.playground/Contents.swift
+++ Swift/3Sum.playground/Contents.swift
@@ -8,15 +8,51 @@
 
 class Solution {
     func threeSum(_ nums: [Int]) -> [[Int]] {
+        var sum: [[Int]] = []
         var numbers = nums
         self.quickSort(&numbers, low: 0, high: nums.count - 1)
         
-        return []
+        var i = 0
+        while i < numbers.count-2 {
+            let value = numbers[i]
+            let twoValues = self.twoSum(numbers, l: i+1, r: numbers.count-1, target: -value)
+            for twoValue in twoValues {
+                var mTwoValue = twoValue
+                mTwoValue.append(value)
+                sum.append(mTwoValue)
+            }
+            
+            repeat { // 跳过重复元素
+                i += 1
+            } while i < numbers.count && value == numbers[i]
+        }
+        return sum
     }
     
-    func twoSum(_ nums: [Int], l: Int, r: Int, sum: Int) -> [[Int]] {
+    func twoSum(_ nums: [Int], l: Int, r: Int, target: Int) -> [[Int]] {
         var sums: [[Int]] = []
-        return []
+        var begin = l, end = r
+        
+        while begin < end {
+            let twoSum = nums[begin] + nums[end]
+            if twoSum < target {
+                begin += 1
+            }
+            else if twoSum > target {
+                end -= 1
+            }
+            else {
+                var arr = [nums[begin], nums[end]]
+                sums.append(arr)
+                while begin < end && nums[begin] == arr[0] {
+                    begin += 1
+                }
+                while begin < end && nums[end] == arr[1] {
+                    end -= 1
+                }
+            }
+        }
+        return sums
     }
     
     func quickSort(_ nums: inout [Int], low: Int, high: Int) {
@@ -26,13 +62,13 @@ class Solution {
         let pivot = nums[low]
         var l = low, h = high
         while l < h {
-            while l < h && nums[h] > pivot {
+            while l < h && nums[h] >= pivot {
                 h -= 1
             }
             if l < h {
                 nums[l] = nums[h]
             }
-            while l < h && nums[l] < pivot {
+            while l < h && nums[l] <= pivot {
                 l += 1
             }
             if l < h {
@@ -47,6 +83,10 @@ class Solution {
 }
 
 let solution = Solution()
+
+var array = [0,0,0]
+solution.threeSum(array)
+
 
 // 暴力法.. 超时
 class Solution1 {

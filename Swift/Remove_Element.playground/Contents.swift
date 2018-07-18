294061e415474cc6f0d568c3593c0d25efa11a37
ShawnFoo <fu4904@gmail.com>
2018-07-18 22:29:30 +0800

    Add Remove Element resolutions

--- Swift/Remove_Element.playground/Contents.swift
+++ Swift/Remove_Element.playground/Contents.swift
@@ -12,9 +12,11 @@ import Foundation
  主要读清楚题目, 题目要求移除所有指定值为val的元素, 并返回新的数组长度.
  特别要求是可修改传入的数组引用, 空间复杂度得为O(1), 不可分配额外的空间创建新的数组.
  
- 所以可用考虑双指针与填坑. 一个记录当前遍历位置, 另一个记录可替换值的坑位, 遍历过程中顺带替换掉所有val
+ 所以可用考虑双指针遍历来填坑
  */
-class Solution {
+
+/// 单向遍历, 一个记录当前遍历位置, 另一个记录可替换值的坑位, 遍历过程中顺带替换掉所有val
+class Solution1 {
     // 时间复杂度O(n), 空间复杂度O(1)
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
         var pitCount = 0 // 坑位数, 用于返回结果计算
@@ -38,3 +40,35 @@ class Solution {
         return nums.count - pitCount
     }
 }
+
+/// Solution1的简化版. 利用慢指针的序号数作为"新数组"的数量
+class SolutionImproved {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var index = 0 // 可以想象成将不等于val的元素添加入数组的过程
+        for elem in nums {
+            if elem != val {
+                nums[index] = elem
+                index += 1
+            }
+        }
+        return index
+    }
+}
+
+/// 前后双向遍历. 末尾元素倒序填入坑位. 因题意没要求结果有序, 故仅需移除指定值的元素即可.
+class Solution2 {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var i = 0
+        var n = nums.count
+        while i < n {
+            if nums[i] == val { // 末尾元素用于填充坑位. 同时数量减一
+                nums[i] = nums[n - 1]
+                n -= 1
+            } else {
+                i += 1
+            }
+        }
+        return n
+    }
+}
+

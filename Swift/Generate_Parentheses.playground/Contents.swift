8d070c6d3a2966a03a7228b54e350ec5958af6e3
ShawnFoo <fu4904@gmail.com>
2018-07-24 22:06:53 +0800

    Generate Parentheses Add Solution

--- Swift/Generate_Parentheses.playground/Contents.swift
+++ Swift/Generate_Parentheses.playground/Contents.swift
@@ -8,6 +8,40 @@ import Foundation
  https://leetcode.com/problems/generate-parentheses/description/
  */
 
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+        var result: [String] = []
+        if n >= 1 {
+            _generateParenthesis(&result, "", n, 0, n)
+        }
+        return result
+    }
+    
+    /// 递归生成包含合法括号对的字符串
+    ///
+    /// - Parameters:
+    ///   - result: 结果数组
+    ///   - str: 括号对字符串
+    ///   - openCount: 开括号剩余个数
+    ///   - rightCount: 闭括号剩余个数
+    ///   - n: 括号对数
+    private func _generateParenthesis(_ result: inout [String],
+                                      _ str: String,
+                                      _ openCount: Int,
+                                      _ rightCount: Int,
+                                      _ n: Int) {
+        if str.count == 2 * n {
+            result.append(str)
+        } else {
+            if openCount > 0 { // 每添加一个开括号, 剩余可添加开括号数减一, 应增加闭括号数加一
+                _generateParenthesis(&result, str + "(", openCount - 1, rightCount + 1, n)
+            }
+            if rightCount > 0 {
+                _generateParenthesis(&result, str + ")", openCount, rightCount - 1, n)
+            }
+        }
+    }
+}
 
 /*
  思路: 排列组合包含所有合法括号对的字符串. 想到回溯, 尝试组合所有可能的情况.
@@ -22,10 +56,10 @@ import Foundation
  第三次: 把所有包含占位字符的字符串继续传入特定方法, 继续生成组合.
  第n次: 重复上述操作, 直至所有字符串都不包含占位字符, 则说明所有组合都已生成. 返回结果
  */
-class Solution {
+class Solution1 {
     private static let placeholder = Character("0")
-    private static let leftBracket = Character("(")
-    private static let rightBracket = Character(")")
+    private static let openBracket = Character("(")
+    private static let closeBracket = Character(")")
     
     func generateParenthesis(_ n: Int) -> [String] {
         if n == 0 {
@@ -33,13 +67,13 @@ class Solution {
         } else if n == 1 {
             return ["()"]
         } else {
-            var result = _generateParenthesis(String(repeating: Solution.placeholder, count: n * 2)) // n对括号占n*2个位置
+            var result = _generateParenthesis(String(repeating: Solution1.placeholder, count: n * 2)) // n对括号占n*2个位置
             var finished = false // 是否所有字符串都组合完成
             while finished != true {
                 finished = true
                 var tempResult: [String] = []
                 for setStr in result {
-                    if setStr.contains(Solution.placeholder) { // 如果有包含placeholder的字符串, 说明还没有组合完
+                    if setStr.contains(Solution1.placeholder) { // 如果有包含placeholder的字符串, 说明还没有组合完
                         finished = false
                         tempResult.append(contentsOf: _generateParenthesis(setStr))
                     } else {
@@ -60,12 +94,12 @@ class Solution {
         var result: [String] = []
         var chars = Array(str)
         for i in 0..<chars.count {
-            if chars[i] == Solution.placeholder { // 找到第一个placeholder的位置
+            if chars[i] == Solution1.placeholder { // 找到第一个placeholder的位置
                 var span = 1 // 右括号间隔 1、3、5..n个位置
                 while i + span < chars.count {
                     var insertable = true
                     for j in i+1...i+span { // 检测左括号到插入右括号的位置间有没有括号, 有的话说明不可插入右括号
-                        if chars[j] == Solution.leftBracket || chars[j] == Solution.rightBracket {
+                        if chars[j] == Solution1.openBracket || chars[j] == Solution1.closeBracket {
                             insertable = false
                             break
                         }
@@ -74,8 +108,8 @@ class Solution {
                         break
                     }
                     var replacedChars = chars
-                    replacedChars[i] = Solution.leftBracket
-                    replacedChars[i + span] = Solution.rightBracket
+                    replacedChars[i] = Solution1.openBracket
+                    replacedChars[i + span] = Solution1.closeBracket
                     result.append(String(replacedChars))
                     span += 2
                 }

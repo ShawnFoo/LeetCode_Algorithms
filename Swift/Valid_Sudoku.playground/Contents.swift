6f92ef6d3e8a994ac95a6f75a98e7e22c017748a
ShawnFoo <fu4904@gmail.com>
2018-07-29 11:10:21 +0800

    Valid Sudoku Add Solution

--- Swift/Valid_Sudoku.playground/Contents.swift
+++ Swift/Valid_Sudoku.playground/Contents.swift
@@ -6,22 +6,74 @@ import Foundation
  Tags: Hash Table
  
  https://leetcode.com/problems/valid-sudoku/description/
+ 
+ 思路: 把大区域划分成9个(3x3)的子区域遍历, 识别子区域合法的同时, 判断所在行列是否出现重复数字
+ 
+ 列|行 0   1   2
+ 0    0 | 1 | 2
+ 1    3 | 4 | 5
+ 2    6 | 7 | 8
+ 
  */
 
 /**
- 思路: 大区域划分成9个(3x3)的子区域遍历, 识别子区域合法的同时, 判断所在行列是否出现重复数字
+ 时间复杂度: O(1). 比下方Solution1(同时间复杂度)使用hash table跑出来的runtime时间要短, 应该是生成key的操作导致的
  
- 行  0   1   2
- 列
- 0   0 | 1 | 2
- 1   3 | 4 | 5
- 2   6 | 7 | 8
+ 使用数组来存储按位操作后的数值. 按位与若大于0则说明存在重复数.
+ 
+ 注释可参考Solution1, 只是空间存储不一样
+ */
+class Solution {
+    private static let dotChar = Character(".")
+    
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        // 以下数组都存储 按位或后的数值 1<<0 | 1<<1
+        var rowsBits: [Int] = Array(repeating: 0, count: 9) // 各行
+        var columnsBits: [Int] = Array(repeating: 0, count: 9) // 各列
+        var boxBits = 0 // 小区域快
+        var boxIndex = 0
+        while boxIndex < 9 {
+            let columnBoxIndex = boxIndex % 3
+            let rowBoxIndex = boxIndex / 3
+            for row in 0..<3 {
+                let rIndex = rowBoxIndex * 3 + row
+                for column in 0..<3 {
+                    let cIndex = columnBoxIndex * 3 + column
+                    let value = board[rIndex][cIndex]
+                    if value == Solution.dotChar {
+                        continue
+                    }
+                    if exists(of: value, with: &boxBits)
+                        || exists(of: value, with: &(rowsBits[rIndex]))
+                        || exists(of: value, with: &(columnsBits[cIndex])) {
+                        return false
+                    }
+                }
+            }
+            boxBits = 0
+            boxIndex += 1
+        }
+        return true
+    }
+    
+    private func exists(of value: Character, with bits: inout Int) -> Bool {
+        let bitValue = 1 << Int(String(value))!
+        if bitValue & bits == 0 {
+            bits += bitValue
+            return false
+        }
+        return true
+    }
+}
+
+/**
+ 时间复杂度: O(1)
  
  使用一个hash table进行记录, key规范为两种形式:
  1. 完整区域的行列Key, 分别记为c_列数_1(column_数值), r_行_2(row_数值)
  2. 子区域Key, 记为 0~8 b_区域号_数值.
  */
-class Solution {
+class Solution1 {
     private static let dotChar = Character(".")
     
     func isValidSudoku(_ board: [[Character]]) -> Bool {
@@ -36,11 +88,11 @@ class Solution {
                 for row in 0..<3 { // 9x9行号
                     let rIndex = rowBox * 3 + row // 9x9行号
                     let value = board[rIndex][cIndex]
-                    if value != Solution.dotChar {
+                    if value != Solution1.dotChar {
                         // 分别生成完整区域的行、列Key以及子区域Key
-                        let rowKey = "r_\(rowBox * 3 + row)_\(value)"
-                        let columnKey = "c_\(columnBox * 3 + column)_\(value)"
-                        let subBoxKey = "b_\(subBoxIndex)_\(value)"
+                        let rowKey = String("r_\(rowBox * 3 + row)_\(value)")
+                        let columnKey = String("c_\(columnBox * 3 + column)_\(value)")
+                        let subBoxKey = String("b_\(subBoxIndex)_\(value)")
                         if checkExistence(of: rowKey, in: &hashTable)
                         || checkExistence(of: columnKey, in: &hashTable)
                         || checkExistence(of: subBoxKey, in: &hashTable) {

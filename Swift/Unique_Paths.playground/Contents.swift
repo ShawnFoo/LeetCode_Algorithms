2029a00d5c222726c3e62b3c51d1c4447befcb24
ShawnFoo <fu4904@gmail.com>
2018-08-11 00:33:19 +0800

    Unique Paths DP Solution

--- Swift/Unique_Paths.playground/Contents.swift
+++ Swift/Unique_Paths.playground/Contents.swift
@@ -9,11 +9,11 @@ import Foundation
  */
 
 /**
- 基于动态规划公式的递归 + Hash Table备忘. 12ms
+ 递归 + Hash Table备忘. 12ms
 
  时间复杂度: O(n). 空间复杂度: O(n)
  
- 思路: 因为每个点只有两种路径选择, 所以从终点来看, 可以推导总路径数 F(m, n) = F(m-1, n) + F(m, n-1) 之和
+ 思路: 因为每个点只有两种路径选择, 所以从终点来看, 可以推导总路径数
  */
 class Solution {
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
@@ -41,7 +41,42 @@ class Solution {
     }
 }
 
-/// 暴力回溯递归实现. 超时. 时间复杂度: O(n^2), 空间复杂度: O(n)
+/**
+ 动态规划. 时间复杂度O(n). 空间复杂度O(n). 12ms
+
+ 思路: 基于前边递归算法实现推导出的公式: F(m, n) = F(m-1, n) + F(m, n-1) 走到某个位置的路径数 = 上边位置的路径数 + 左边位置的路径数
+ 
+ 将多个维度拆分成1个维度的动态规划问题, 一行一行de去处理. 第一行以及第一列上的点只能走直线到达, 故这些点的路径数统统为1
+ */
+class SolutionDP {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        if m == 0 || n == 0 {
+            return 0
+        }
+        var memo: [Int: Int] = [1: 1]
+        // 直接从第二行, 第二列开始. 跳过固定路径的格子m+n个
+        var row = 2
+        while row <= n {
+            var column = 2
+            while column <= m {
+                let key = _memoHashKey(column, row)
+                memo[key] = memo[_memoHashKey(column - 1, row)]! + memo[_memoHashKey(column, row - 1)]!
+                column += 1
+            }
+            row += 1
+        }
+        return memo[_memoHashKey(m, n)]!
+    }
+    
+    private func _memoHashKey(_ m: Int, _ n: Int) -> Int {
+        if m == 1 || n == 1 {
+            return 1 // 第一行或第一列的点全部都只有1种路径走法, 共用1个key
+        }
+        return m + n * 100
+    }
+}
+
+/// 暴力回溯递归实现. 超时. 时间复杂度: O(2^n), 空间复杂度: O(n)
 class SolutionBacktracking {
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
         if m == 0 || n == 0 {
@@ -72,6 +107,7 @@ class SolutionBacktracking {
     }
 }
 
-Solution().uniquePaths(3, 2)
-Solution().uniquePaths(7, 3)
-Solution().uniquePaths(23, 12)
+//Solution().uniquePaths(3, 2)
+//Solution().uniquePaths(7, 3)
+//Solution().uniquePaths(23, 12)
+SolutionDP().uniquePaths(3, 3)

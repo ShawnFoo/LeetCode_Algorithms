f02083db9409362a1594819fa038988a32e296a8
ShawnFoo <fu4904@gmail.com>
2018-08-20 21:17:18 +0800

    Permutations Add DFS Solution

--- Swift/Permutations.playground/Contents.swift
+++ Swift/Permutations.playground/Contents.swift
@@ -1,12 +1,65 @@
 import Foundation
 
 /**
- Permutations
+ 46. Permutations
  
  Tags: Backtracking
  
  https://leetcode.com/problems/permutations/description/
  */
+
+/// 12ms. DFS回溯, 相比于后边的SolutionBacktracking, 减少了反复创建新数组的消耗, 仅用一个数组, 每次把后边一个数交换到当前遍历的位置上来
+class SolutionBacktrackingDFS {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        if nums.count > 1 {
+            var mNums = nums
+            var results: [[Int]] = []
+            backtracking(&mNums, start: 0, &results)
+            return results
+        }
+        return [nums]
+    }
+    
+    private func backtracking(_ nums: inout [Int], start: Int, _ results: inout [[Int]]) {
+        if start == nums.count - 1 {
+            results.append(nums)
+        } else if start < nums.count - 1 {
+            for i in start..<nums.count {
+                nums.swapAt(start, i)
+                backtracking(&nums, start: start + 1, &results)
+                nums.swapAt(start, i)
+            }
+        }
+    }
+}
+
+/// 回溯递归实现
+class SolutionBacktracking {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        if nums.count > 1 {
+            var result: [[Int]] = []
+            _permute([], nums, &result)
+            return result
+        }
+        return [nums]
+    }
+    
+    private func _permute(_ arr: [Int], _ nums: [Int], _ result: inout [[Int]]) {
+        if arr.count == nums.count {
+            result.append(arr)
+        } else {
+            for value in nums {
+                var toArr = arr
+                if toArr.contains(value) {
+                    continue
+                } else {
+                    toArr.append(value)
+                    _permute(toArr, nums, &result)
+                }
+            }
+        }
+    }
+}
 
 /// 常规排列组合递归实现版
 class SolutionRecursive {
@@ -40,30 +93,3 @@ class SolutionRecursive {
     }
 }
 
-/// 回溯递归实现
-class SolutionBacktracing {
-    func permute(_ nums: [Int]) -> [[Int]] {
-        if nums.count > 1 {
-            var result: [[Int]] = []
-            _permute([], nums, &result)
-            return result
-        }
-        return [nums]
-    }
-
-    private func _permute(_ arr: [Int], _ nums: [Int], _ result: inout [[Int]]) {
-        if arr.count == nums.count {
-            result.append(arr)
-        } else {
-            for value in nums {
-                var toArr = arr
-                if toArr.contains(value) {
-                    continue
-                } else {
-                    toArr.append(value)
-                    _permute(toArr, nums, &result)
-                }
-            }
-        }
-    }
-}

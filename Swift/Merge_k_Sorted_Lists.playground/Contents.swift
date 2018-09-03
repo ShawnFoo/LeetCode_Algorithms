154df992dcaeb9673d83a74f6e5bb58a9f5adb36
ShawnFoo <fu4904@gmail.com>
2018-09-03 23:11:30 +0800

    Merge k Sorted Lists Add D&C Solution

--- Swift/Merge_k_Sorted_Lists.playground/Contents.swift
+++ Swift/Merge_k_Sorted_Lists.playground/Contents.swift
@@ -9,7 +9,7 @@ import Foundation
  */
 
 /**
- HashMap实现, 72ms.
+ HashMap实现, 72ms. 时间复杂度O(2N), 空间复杂度O(n)
  
  思路: 基于解决hash冲突的链地址法. 节点val作为key, 后边值冲突的节点将追加在上一个同值节点的后边
  */
@@ -44,3 +44,82 @@ class Solution {
         return retHead
     }
 }
+
+/**
+ 分治实现, 76ms. 时间复杂度:O(N*Logk), 空间复杂度O(n)
+ 
+ 思路: 两个两个进行合并(共log2 k 次), 直至仅剩一个. 相比于每次合并出一个再接着合并的实现(合并后的链表长度不断增加), 分治省去了许多的重复遍历
+ 
+ 空间方面, 因为Swift参数为let常量不可修改, 这部分没法优化
+ */
+class SolutionDC {
+    func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+        if lists.count == 0 {
+            return nil
+        }
+        var results = lists
+        var temps: [ListNode?] = []
+        while results.count > 1 {
+            temps.append(contentsOf: results)
+            results.removeAll()
+            var i = 0
+            while i < temps.count {
+                if i + 1 < temps.count {
+                    results.append(mergeTwoLists(temps[i], temps[i + 1]))
+                } else {
+                    results.append(temps[i])
+                }
+                i += 2
+            }
+            temps.removeAll()
+        }
+        return results.first!
+    }
+    
+    func mergeTwoLists(_ lhs: ListNode?, _ rhs: ListNode?) -> ListNode? {
+        var prevNode: ListNode? = nil
+        var lNode = lhs
+        var rNode = rhs
+        while lNode != nil || rNode != nil {
+            if let left = lNode, let right = rNode {
+                if left.val <= right.val {
+                    prevNode?.next = left
+                    prevNode = left
+                    lNode = left.next
+                } else {
+                    prevNode?.next = right
+                    prevNode = right
+                    rNode = right.next
+                }
+            } else {
+                prevNode?.next = lNode ?? rNode
+                break
+            }
+        }
+    
+        if lhs != nil && rhs != nil {
+            return lhs!.val <= rhs!.val ? lhs : rhs
+        } else {
+            return lhs != nil ? lhs : rhs
+        }
+    }
+}
+
+var node1 = ListNode(1)
+var node2 = ListNode(4)
+var node3 = ListNode(5)
+var node4 = ListNode(1)
+var node5 = ListNode(3)
+var node6 = ListNode(4)
+var node7 = ListNode(2)
+var node8 = ListNode(6)
+
+node1.next = node2
+node2.next = node3
+node4.next = node5
+node5.next = node6
+node7.next = node8
+
+let tests = [node1, node4, node7]
+
+SolutionDC().mergeKLists(tests)!

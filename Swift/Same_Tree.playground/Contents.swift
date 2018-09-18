6f8003be702aa938c1adc2700f7ccfbe697270fc
ShawnFoo <fu4904@gmail.com>
2018-09-18 23:27:58 +0800

    Same Tree Add Non-Recursive Solution

--- Swift/Same_Tree.playground/Contents.swift
+++ Swift/Same_Tree.playground/Contents.swift
@@ -9,6 +9,54 @@ import Foundation
  */
 
 /**
+ 非递归 先序遍历实现, 12 ms. 时间复杂度O(n), 空间复杂度O(n)
+ */
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        var pStack: [TreeNode] = []
+        var qStack: [TreeNode] = []
+        
+        var pNode = p
+        var qNode = q
+        while pNode != nil || qNode != nil || pStack.count > 0 || qStack.count > 0 {
+            if pNode != nil || qNode != nil {
+                if let lhs = pNode, let rhs = qNode {
+                    if lhs.val == rhs.val {
+                        var count = 0
+                        if lhs.right != nil {
+                            qStack.append(lhs.right!)
+                            count += 1
+                        }
+                        if rhs.right != nil {
+                            pStack.append(rhs.right!)
+                            count += 1
+                        }
+                        if count != 2 && count != 0 {
+                            return false
+                        }
+                        pNode = lhs.left
+                        qNode = rhs.left
+                    } else {
+                        return false
+                    }
+                } else {
+                    return false
+                }
+            } else if pStack.count > 0 || qStack.count > 0 {
+                if pStack.count != qStack.count {
+                    return false
+                } else {
+                    pNode = pStack.popLast()
+                    qNode = qStack.popLast()
+                }
+            }
+        }
+        
+        return true
+    }
+}
+
+/**
  递归先序遍历, 12ms. 时间复杂度O(n), 空间复杂度O(n)
  */
 class SolutionRecursive {

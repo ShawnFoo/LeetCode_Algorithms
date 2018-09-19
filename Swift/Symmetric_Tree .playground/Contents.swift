7a2c4f548728fff1f38207639709e8a5deac4b3e
ShawnFoo <fu4904@gmail.com>
2018-09-19 22:06:54 +0800

    Symmetric Tree

---
+++ Swift/Symmetric_Tree .playground/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+/**
+ 101. Symmetric Tree
+ 
+ Tags: Tree、DFS、BFS
+ 
+ https://leetcode.com/problems/symmetric-tree/description/
+ */
+
+/**
+ 递归dfs实现, 16ms. 时间复杂度O(n/2-1), 空间复杂度O(n)
+ 
+ 思路: 左右子树上的节点同时对称遍历
+ */
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return recursive(root?.left, root?.right)
+    }
+    
+    private func recursive(_ lhs: TreeNode?, _ rhs: TreeNode?) -> Bool {
+        var isSame = lhs?.val == rhs?.val
+        if isSame && (lhs?.left != nil || rhs?.right != nil) {
+            isSame = recursive(lhs?.left, rhs?.right)
+        }
+        if isSame && (lhs?.right != nil || rhs?.right != nil) {
+            isSame = recursive(lhs?.right, rhs?.left)
+        }
+        return isSame
+    }
+}

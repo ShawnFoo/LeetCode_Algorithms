b26829c5c8c23bd49661da906364f7e27eea6f8c
ShawnFoo <fu4904@gmail.com>
2018-09-23 11:05:06 +0800

    lowestCommonAncestor Add non-recursive Solution

--- C++/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
+++ C++/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -15,12 +15,27 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/*
- 递归实现, 16ms
- 
- 思路: BST特性, 左子树上的节点必定比当前节点上的上的小; 右子树, 同理.
+// 思路: BST特点, 左子树上的节点必定比当前节点上的上的小; 右子树, 同理.
+
+/**
+ 非递归实现, 16ms. 时间复杂度O(logn), 空间复杂度O(1)
  */
 class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int minVal = p->val <= q->val ? p->val : q->val;
+        int maxVal = p->val <= q->val ? q->val : p->val;
+        while (!(root->val >= minVal && root->val <= maxVal)) {
+            root = root->val < minVal ? root->right : root->left;
+        }
+        return root;
+    }
+};
+
+/**
+ 递归实现, 16~28ms(先判断root->val > minVal时会短点..测试用例问题)
+ */
+class SolutionRecursive {
     public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         int maxVal = p->val >= q->val ? p->val : q->val;

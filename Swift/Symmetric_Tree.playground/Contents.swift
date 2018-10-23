db1de890db7f3c2337dd9f5616ad98ffeeeab608
ShawnFoo <fu4904@gmail.com>
2018-10-23 23:42:07 +0800

    Symmetric Tree Add Solution

--- Swift/Symmetric_Tree.playground/Contents.swift
+++ Swift/Symmetric_Tree.playground/Contents.swift
@@ -29,3 +29,20 @@ class Solution {
         return isSame
     }
 }
+
+class SolutionBSF {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return recursive(root?.left, root?.right)
+    }
+    
+    private func recursive(_ lhs: TreeNode?, _ rhs: TreeNode?) -> Bool {
+        var isSame = lhs?.val == rhs?.val
+        if isSame && (lhs?.left != nil || rhs?.right != nil) {
+            isSame = recursive(lhs?.left, rhs?.right)
+        }
+        if isSame && (lhs?.right != nil || rhs?.right != nil) {
+            isSame = recursive(lhs?.right, rhs?.left)
+        }
+        return isSame
+    }
+}

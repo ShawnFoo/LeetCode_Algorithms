3a3201e2add3b503a173d14e68a9d1f48ebef516
ShawnFoo <fu4904@gmail.com>
2018-10-27 21:15:28 +0800

    Maximum Subarray Update Solution

--- Swift/Maximum_Subarray.playground/Contents.swift
+++ Swift/Maximum_Subarray.playground/Contents.swift
@@ -3,13 +3,13 @@ import Foundation
 /**
  53. Maximum Subarray
  
- Tags: Array、Divide and Conquer、Dynamic Programming、
+ Tags: Array、Divide and Conquer、Dynamic Programming
  
  https://leetcode.com/problems/maximum-subarray/description/
  */
 
 /**
- Kadane算法(按贪心算法去理解), 时间复杂度O(n), 空间复杂度O(n)
+ Kadane算法(按贪心算法去理解), 时间复杂度O(n), 空间复杂度O(1)
 
  求连续数字组成和最大的子数组, 求连续的局部最优解, 遍历下来最大的局部之和即为最终结果
  */
@@ -18,43 +18,46 @@ class Solution {
         if nums.count == 0 {
             return 0
         }
-        var curSum = nums[0] // 局部连续子序列和
-        var maxSum = nums[0] // 全局遍历中的最大和
+        var curSum = nums[0] // 局部最大连续组合值
+        var maxSum = nums[0] // 全局最大连续组合值
         for i in 1..<nums.count {
             curSum = max(nums[i], nums[i] + curSum) // 若加上当前数(主要是负数)导致和还小于当前数, 则取当前值
-            maxSum = max(maxSum, curSum) // 不断更新局部最优解
+            maxSum = max(maxSum, curSum) // 更新全局最优解
         }
         return maxSum
     }
 }
 
 /**
- 动态规划, 时间复杂度O(n), 空间复杂度O(n)
+ 动态规划, 时间复杂度O(n), 空间复杂度O(1)
  
  把大问题分解为求包含第n个元素的局部连续序列和, 然后求出包含各个位置元素的子序列和. 取最大的一个即为结果.
  
- 包含第n个元素的最大和公式为 Sum(n) = (Sum(n-1) < 0 ? 0 : Sum(n-1)) + nums[n]
+ 包含第n个元素的最大和公式为 Sum(n) = max(0, Sum(n-1)) + nums[n]
  
  Sum(n-1)小于0, 即为负数, 则加上只会更小, 所以取当前nums[n]重新开始
  */
 class SolutionDP {
     func maxSubArray(_ nums: [Int]) -> Int {
-        var curSum = 0 // 必须包含当前第n个元素的最大序列和
-        var maxSum = nums.first ?? 0 // 全局遍历中的最大和
-        for i in 0..<nums.count {
-            curSum = (curSum < 0 ? 0 : curSum) + nums[i]
-            maxSum = max(curSum, maxSum)
+        if !nums.isEmpty {
+            var sumWithN = nums.first! // 包含第N个元素的连续组合值
+            var maxSum = sumWithN // 全局最大连续组合值
+            for i in 1..<nums.count {
+                sumWithN = max(0, sumWithN) + nums[i]
+                maxSum = max(maxSum, sumWithN)
+            }
+            return maxSum
         }
-        return maxSum
+        return 0
     }
 }
 
 /**
- 分治法
+ 分治法实现, 类似merge sort, 分+合
  
- 通过分治法, 将整个范围不断划分为小的子区间至最小单位1个元素, 然后将子区间不断合并(通过相邻子区间的中点), 求得合并下来后最大的子区间
+ 将整个范围不断划分为小的子区间至最小单位1个元素, 然后将子区间不断合并(通过相邻子区间的中点), 求得合并下来后最大的子区间
  
- 可以肯定的是, 和最大的子序列必定在 0~nums.count-1范围内. 通过分治, 可以确定子序列一定会位于划分后子区间的以下范围内
+ 可以肯定的是, 和最大的子序列必定在 0~n-1 范围内. 通过分治, 可以确定子序列一定会位于划分后子区间的以下范围内
  
  left ~ mid-1 | mid+1 ~ right | left + right (mid位于left~right)
  */
@@ -72,6 +75,7 @@ class SolutionDC {
         let mid = (left + right) / 2
         let leftMax = _maxSubArray(nums, left, mid - 1)
         let rightMax = _maxSubArray(nums, mid + 1, right)
+        
         var mLeftMax = 0 // 包含mid位置的左侧最大和
         var mRightMax = 0 // 包含mid位置的右侧最大和
         var sum = 0
@@ -94,5 +98,5 @@ class SolutionDC {
 }
 
 //Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
-//SolutionDP().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
-SolutionDC().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+SolutionDP().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+//SolutionDC().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])

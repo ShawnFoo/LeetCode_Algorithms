8d523050216dba21a580b0d1bc49b580d21ddc11
ShawnFoo <fu4904@gmail.com>
2018-10-31 01:38:19 +0800

    Swap Nodes in Pairs Add Solution

--- Swift/Swap_Nodes_in_Pairs.playground/Contents.swift
+++ Swift/Swap_Nodes_in_Pairs.playground/Contents.swift
@@ -8,8 +8,51 @@ import Foundation
  https://leetcode.com/problems/swap-nodes-in-pairs/description/
  */
 
-/// 递归实现. 思路: 每两个节点为一对进行调换, 并指向后续一对节点调换后的前节点, 直至最后仅剩下一个节点或为空
+/**
+ 基本反转链表通用实现, 16ms.
+ 
+ 思路: 分治, 拆分为特定长度的某段子链表进行反转, 再pairs相连合并即可
+ */
 class Solution {
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        let kLength = 2 // 反转长度
+        let result = reverseList(head, kLength)
+        if result.tail == nil {
+            return result.head
+        } else {
+            let retHead = result.head
+            var lastResult = result
+            while lastResult.tail != nil {
+                let result = reverseList(lastResult.tail!.next, kLength)
+                lastResult.tail!.next = result.head
+                lastResult = result
+            }
+            return retHead
+        }
+    }
+
+    private func reverseList(_ head: ListNode?, _ length: Int) -> (head: ListNode?, tail: ListNode?) {
+        if head == nil || head!.next == nil {
+            return (head: head, tail: nil)
+        }
+        
+        var retHead = head
+        var limit = length - 1
+        while limit > 0 && head!.next != nil {
+            let next = head!.next
+            head!.next = next!.next
+            next!.next = retHead
+            retHead = next
+            
+            limit -= 1
+        }
+        
+        return (head: retHead, tail: head)
+    }
+}
+
+/// 递归实现. 思路: 每两个节点为一对进行调换, 并指向后续一对节点调换后的前节点, 直至最后仅剩下一个节点或为空
+class Solution1 {
     func swapPairs(_ head: ListNode?) -> ListNode? {
         if let aheadNode = head, let behindNode = head?.next {
             let nextHead = behindNode.next
@@ -21,28 +64,26 @@ class Solution {
     }
 }
 
-/// 思路: 双指针顺序遍历替换
-class Solution1 {
+/// 12ms. 思路: 双指针顺序遍历替换
+class Solution2 {
     func swapPairs(_ head: ListNode?) -> ListNode? {
-        var retHead = head
-        var curNode = head // 当前遍历位置节点
-        var tailNode: ListNode? = nil // 新列表的尾节点
-        while curNode != nil { // 两两遍历
-            let aheadNode = curNode
-            let behindNode = curNode?.next
-            if aheadNode === head && head?.next != nil { // 头结点更新
-                retHead = head?.next
-            }
-            // 遍历位置更新
-            curNode = curNode?.next?.next
-            // 尾节点追加当前换位的节点. 若仅剩一个节点则指向前边那个
-            tailNode?.next = behindNode ?? aheadNode
-            // 后边指向前边
-            behindNode?.next = aheadNode
-            
-            // 尾节点更新
-            tailNode = aheadNode
-            tailNode?.next = nil
+        if head == nil || head!.next == nil {
+            return head
+        }
+        let retHead = head!.next
+        var tail: ListNode? = nil // 上对pairs的tail
+        var cur = head
+        while cur?.next != nil {
+            let next = cur!.next
+            let nextHead = next?.next
+            // pairs相连
+            tail?.next = next
+            // pairs转换
+            next?.next = cur
+            cur!.next = nextHead
+            // tail记录, 移至下一对
+            tail = cur
+            cur = nextHead
         }
         return retHead
     }
